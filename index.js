5f03557fb01a9254df3c8815a21f5545e2414fff
Christian Repnik <kris18star@gmail.com>
2024-02-19 00:58:00 +0300

    Only polling

--- index.js
+++ index.js
@@ -13,21 +13,9 @@ const io = new Server(httpServer, { cors: URL });
 io.on("connection", (socket) => {
   console.log("server connected")
 
-  socket.on('beginPath', (arg) => {
-    socket.broadcast.emit('beginPath' + arg.id, arg)
-  })
-
-  socket.on('drawLine', (arg) => {
-    socket.broadcast.emit('drawLine' + arg.id, arg)
-  })
-
   socket.on('drawPolling', (arg) => {
     console.log(arg);
     socket.broadcast.emit('drawPolling' + arg.id, arg)
-  })
-
-  socket.on('changeConfig', (arg) => {
-    socket.broadcast.emit('changeConfig' + arg.id, arg)
   })
 });
 
